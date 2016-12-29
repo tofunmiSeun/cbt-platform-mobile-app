5d029c56959d7eb565d2ba8de2a2db7a0a1cb125
Ogungbaigbe Tofunmi <tofunmiseun@gmail.com>
2016-12-29 13:17:47 +0100

    implemented timer for tests.

--- app/src/main/java/com/unilorin/vividmotion/pre_cbtapp/activities/TakeQuizActivity.java
+++ app/src/main/java/com/unilorin/vividmotion/pre_cbtapp/activities/TakeQuizActivity.java
@@ -1,6 +1,7 @@
 package com.unilorin.vividmotion.pre_cbtapp.activities;
 
 import android.os.AsyncTask;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -21,6 +23,7 @@ import com.unilorin.vividmotion.pre_cbtapp.views.adapters.QuestionsRecyclerViewA
 
 import java.util.List;
 import java.util.Map;
+import java.util.TimerTask;
 
 public class TakeQuizActivity extends AppCompatActivity {
 
@@ -29,6 +32,9 @@ public class TakeQuizActivity extends AppCompatActivity {
     private RecyclerView questionsRecyclerView;
     private QuestionsRecyclerViewAdapter questionsRecyclerViewAdapter;
     private FloatingActionButton fab;
+    private TextView timerTextView;
+    private CountDownTimer timer;
+    private boolean testSubmitted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +42,7 @@ public class TakeQuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_take_quiz);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        timerTextView = (TextView) findViewById(R.id.timerTextView);
         fab = (FloatingActionButton) findViewById(R.id.fab);
 
         String courseObjectJson = getIntent().getStringExtra("courseForQuiz");
@@ -43,6 +50,14 @@ public class TakeQuizActivity extends AppCompatActivity {
         setTitle(courseForQuiz.getCourseCode());
 
         new GetTestQuestionsTask().execute();
+    }
+
+    String timeFormat(long remainingSeconds){
+        String s;
+        long minutes = remainingSeconds / 60;
+        long seconds = remainingSeconds % 60;
+        s = String.format("%02d:%02d", minutes, seconds);
+        return s;
     }
 
     private class GetTestQuestionsTask extends AsyncTask<Void, Void, Void>{
@@ -66,12 +81,31 @@ public class TakeQuizActivity extends AppCompatActivity {
             questionsRecyclerViewAdapter = new QuestionsRecyclerViewAdapter(testQuestions, TakeQuizActivity.this);
             questionsRecyclerView.setAdapter(questionsRecyclerViewAdapter);
 
+            long testTime = 2 * 60 *1000L;
+            timer = new CountDownTimer(testTime, 1000) {
+
+                public void onTick(long millisUntilFinished) {
+                    timerTextView.setText(timeFormat(millisUntilFinished / 1000));
+                }
+
+                public void onFinish() {
+                    if (!testSubmitted) {
+                        timerTextView.setText(timeFormat(0));
+                        Toast.makeText(getBaseContext(), "Time up!", Toast.LENGTH_SHORT).show();
+                        fab.callOnClick();
+                    }
+                }
+
+            };
+
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     // TODO: 27/12/2016 Submit Quiz
 
                     final Map<String, Integer> params = questionsRecyclerViewAdapter.mark();
+                    testSubmitted = true;
+                    timer.cancel();
 
                     fab.setImageResource(R.drawable.ic_assessment_white_24dp);
 
@@ -91,9 +125,10 @@ public class TakeQuizActivity extends AppCompatActivity {
                             dialogFragment.show(getSupportFragmentManager(), "dialog");
                         }
                     });
-
                 }
             });
+
+            timer.start();
         }
     }
 }
